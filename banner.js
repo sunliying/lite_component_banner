e9e41767792b05e313dee83b13be801fba5bff90
sunliying <1174587076@qq.com>
2015-10-28 17:14:15 +0800

    the forth one ,and mentor zhao,I can't remove multiple class in a time, which is annoying

--- banner.js
+++ banner.js
@@ -11,42 +11,30 @@ define(["bower_components/litejs/lite"],function(l){
 		images = document.querySelectorAll(".image");
 		p_btn = document.querySelector(".prev");
 		n_btn = document.querySelector(".next");
-		dots = document.querySelector(".dots");
+		dots = document.querySelectorAll(".dots li");
 	var	current = 0;
 	var next= 1;
 	var	animating = false;
 	var flag;
+	var classArray=[
+	["image_current","image_current_next","image_current_pre", "image_next","image_pre"],
+	["color"]
+	];
 	// the current state
-	l.addClass(images[current],'image_current');
-
-
+	l.addClass(images[current],classArray[0][0]);
         //  when the animation is running, make animating ture
 		for (var i = 0; i < images.length; i++) {
-			images[i].addEventListener("webkitAnimationStart",function(){
-				animating =true;
-		})
-			
+		// 	images[i].addEventListener("webkitAnimationStart",function(){
+		// 		animating =true;
+		// })			
 			images[i].addEventListener("webkitAnimationEnd",function(){
 				animating = false;
-		})
-			
+		})			
 		};
-
-		// make sure there are element node
-		var _dots=[];
-
-		for (var i = 0; i < dots.childNodes.length; i++) {
-			if(dots.childNodes[i].nodeType == 1){
-				_dots.push(dots.childNodes[i]);
-			}
-		};
-
 		//the current dot's color
-    l.addClass(_dots[current],"color");		
-		
+    l.addClass(dots[current],"color");				
     //接入接口
-    init = function(){
-			
+    init = function(){			
 		p_btn.addEventListener("click", function(){
 			if (animating){
 				return;
@@ -61,7 +49,6 @@ define(["bower_components/litejs/lite"],function(l){
 			scroll(next,flag);
 			change=window.setInterval(auto,5000);
 		})
-
 		n_btn.addEventListener("click", function(){
 			if (animating){
 				return;
@@ -76,13 +63,10 @@ define(["bower_components/litejs/lite"],function(l){
 			scroll(next,flag);
 			change=window.setInterval(auto,5000);
 		})
-
-		
-
-		for (var i = 0; i < _dots.length; i++) {
+		for (var i = 0; i < dots.length; i++) {
 
 			(function(j){
-				_dots[j].addEventListener("click", function(){
+				dots[j].addEventListener("click", function(){
 					if (animating){
 						return;
 					}
@@ -95,8 +79,6 @@ define(["bower_components/litejs/lite"],function(l){
 			})(i)
 			
 		};
-
-
 		function auto(){
 				if(animating){
 					return;
@@ -109,44 +91,30 @@ define(["bower_components/litejs/lite"],function(l){
 				}
 				scroll(next, flag);
 			}
-
 		change =window.setInterval(auto,5000);	
 }
-
-
 	    scroll = function(next, flag){
-
+	    	animating=true;
 		for (var i = 0; i < images.length; i++) {
-
-			l.removeClass(images[i],"image_current");
-			l.removeClass(images[i],"image_current_next");
-			l.removeClass(images[i],"image_current_pre");
-			l.removeClass(images[i],"image_next");
-			l.removeClass(images[i],"image_pre");
-			l.removeClass(_dots[i],"color");
-
-		};
-	
-		l.addClass(images[current],"image_current");
-		l.addClass(images[next],'image_current');
-		l.addClass(_dots[next],"color");
-
+			l.removeClass(images[i],classArray[0][0]);
+			l.removeClass(images[i],classArray[0][1]);
+			l.removeClass(images[i],classArray[0][2]);
+			l.removeClass(images[i],classArray[0][3]);
+			l.removeClass(images[i],classArray[0][4]);
+			l.removeClass(dots[i],classArray[1][0]);
+		};	
+		l.addClass(images[current],classArray[0][0]);
+		l.addClass(images[next],classArray[0][0]);
+		l.addClass(dots[next],classArray[1][0]);
 		if (flag==1||(flag==2&&current<next)){
-
-			l.addClass(images[current],'image_current_next');
-			l.addClass(images[next],'image_next');
-			
+			l.addClass(images[current],classArray[0][1]);
+			l.addClass(images[next],classArray[0][3]);			
 		}else if(flag==-1||(flag==2&&current>next)){
-
-			l.addClass(images[current],'image_current_pre');
-			l.addClass(images[next],'image_pre');
-			
+			l.addClass(images[current],classArray[0][2]);
+			l.addClass(images[next],classArray[0][4]);			
 		}
-
 			current = next;
-
 	}
-
 	return {
 		init: init
 	}
